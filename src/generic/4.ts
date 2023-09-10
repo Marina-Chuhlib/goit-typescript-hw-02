d45cd77075d7dd789cfbcc6ef6ec1e4f2b7f009b
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-09-10 17:10:31 +0300

    Done generics

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface IComponentProps {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends IComponentProps> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<IComponentProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,11 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface IKeyValuePair<T extends keyof any, U> {
+  key: T;
+  value: U;
 }
+
+type KeyValuePair<T extends keyof any, U> = Record<T, U>;
 
 export {};
